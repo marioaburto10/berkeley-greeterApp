efe76647e88d0761c98f452d603ddd0419d3735d
Mario Aburto <marioaburto10@gmail.com>
2017-12-04 22:19:56 -0500

    created Greeter contract

---
+++ greeter/contracts/Greeter.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract Greeter {
+	string greeting;
+
+	function greeter(string _greeting) public {
+		greeting = _greeting;
+	}
+
+	function greet() constant returns (string) {
+		return greeting;
+		
+	}
+}
