1e3f9df0e9d2c48ad92387273c0b381daa958c31
Mario Aburto <marioaburto10@gmail.com>
2017-12-04 22:27:04 -0500

    set up the config to connect to a network

--- greeter/truffle.js
+++ greeter/truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+	// used to connect to the ethereum network
+	networks: {
+		development: {
+			host: "localhost",
+			port: 8545,
+			network_id: "*" // match any network id
+		}
+	}
 };
